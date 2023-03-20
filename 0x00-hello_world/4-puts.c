25d44e960ffcfd699bd6a993fcd3600cf06ff149
Ubong Obot <obot.kingsley23@gmail.com>
2023-03-20 16:28:44 -0700

    Printing some text using teh puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ubong[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(ubong);
+	return (0);
+}
