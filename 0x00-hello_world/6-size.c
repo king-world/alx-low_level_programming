c5382de77538684193619b81b3466d0eaa78d78a
Ubong Obot <obot.kingsley23@gmail.com>
2023-03-20 18:29:30 -0700

    A script that prints all types of the computer it is compiled and run

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,11 @@
 int main(void)
 {
 	printf("Size of char: %lu byte\n", sizeof(char));
-	printf("Size of int: %lu bytes\n", sizeof(int));
-	printf("Size of short: %lu bytes\n", sizeof(short));
-	printf("Size of long: %lu bytes\n", sizeof(long));
-	printf("Size of float: %lu bytes\n", sizeof(float));
-	printf("Size of double: %lu bytes\n", sizeof(double));
-	printf("Size of long double: %lu bytes\n", sizeof(long double));
-	printf("Size of void: %lu bytes\n", sizeof(void);
+	printf("Size of int: %lu bytes\n", sizeof(int
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s) \n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
