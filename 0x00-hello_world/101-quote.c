0f65ab0ccd0d977f155ec248fb2ac70291ba63a3
Ubong Obot <obot.kingsley23@gmail.com>
2023-03-21 06:19:37 -0700

    Printing some text using the write function in c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,7 @@
  */
 int main(void)
 {
-	char ubong[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-
-	write(STDOUT_FILENO, ubong, sizeof(ubong));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
 }
