642208635901ade50fa13ff54b89b3d22bfa4dbd
Ubong Obot <obot.kingsley23@gmail.com>
2023-03-26 18:16:54 -0700

    Targeting the last number of a random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main -main function
+ *
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	int n;
+	int u;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	u = n % 10;
+
+	if (u > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, u);
+	else if (u == 0)
+		printf("Last digit of %d is %d: and is 0", n, u);
+	else if (u < 6 && u != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, u);
+	printf("\n");
+	return (0);
+}
