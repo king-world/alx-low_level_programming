689fcfd00232eea82b76bd931c07921b06a8f818
Ubong Obot <obot.kingsley23@gmail.com>
2023-03-31 04:52:35 -0700

    Printing all single numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int u;
+
+	for (u = 0; u < 10; u++)
+		printf("%d", u);
+	printf("\n");
+	return (0);
+}
