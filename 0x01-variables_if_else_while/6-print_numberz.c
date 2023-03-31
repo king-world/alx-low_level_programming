af99237885cb6fecb911799319dc3c8a2ff16121
Ubong Obot <obot.kingsley23@gmail.com>
2023-03-31 05:28:24 -0700

    Printing the single digit numbers of base 10 starting from 0 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int u;
+
+	for (u = 0; u < 10; u++)
+		putchar(u + '0');
+	putchar('\n');
+	return (0);
+}
