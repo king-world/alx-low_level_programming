592cafa019eb26a123aa672c5c1091d6bffed586
Ubong Obot <obot.kingsley23@gmail.com>
2023-04-03 17:51:17 -0700

    Print 1 when it is alphabet and 0 when otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * islower - Thie function prints alphabets in lower cases
+ * @c: Paramenter to be printed
+ *
+ * Return: 1 if it is lower case and 0 is different
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
