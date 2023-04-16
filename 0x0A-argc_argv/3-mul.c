f3501b4e577dcd6dbeaf4c377473448563f02952
Ubong Obot <obot.kingsley23@gmail.com>
2023-04-15 20:25:40 -0700

    A program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -32,7 +32,7 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9'
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 
 					break;
 			f = 0;
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = _atoi(argv[1]);
-	num2 = _atoi(argc[2]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
 
