82dc1b2fd50b1944ce6a6fd48b80475e4727ba55
Ubong Obot <obot.kingsley23@gmail.com>
2023-04-15 21:00:48 -0700

    Adding positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * check_num - Check if passed parameter are digitss
+ * @str: array str
+ *
+ * Return: Always 0
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - print program name
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+	if (check_num(argv[count]))
+	{
+		str_to_int = atoi(argv[count]);
+		sum += str_to_int;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
