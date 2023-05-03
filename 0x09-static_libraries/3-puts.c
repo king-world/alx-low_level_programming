bca3e718423b225223ebae784496c5479bcf4c88
Ubong Obot <obot.kingsley23@gmail.com>
2023-05-02 20:46:50 -0700

    A static library libmy.a containing all required functions

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _puts - Main function
+ * @str: String
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
