a3b56ebfc71c19b0f9248086d7d79b057c53fe02
Ubong Obot <obot.kingsley23@gmail.com>
2023-07-13 18:41:24 -0700

    Printing a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,8 @@
  * read_textfile - This reads text file and prints to STDOUT
  * @filename: This is the text file being read
  * @letters: This is the number of letters to be read
- * Return: w- actual number of bytes read and printed 0 when function fails or filename is NULL.
+ * Return: w- actual number of bytes read and printed 0 when function fails
+ * or filename is NULL.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
